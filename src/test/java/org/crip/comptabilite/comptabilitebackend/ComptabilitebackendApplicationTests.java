5b3bf578df39dd4f4a761c5c42d90e56ae26d963
nationaldata <nationaldatasystem@gmail.com>
2019-09-14 11:13:01 +0200

    first commit

---
+++ src/test/java/org/crip/comptabilite/comptabilitebackend/ComptabilitebackendApplicationTests.java
@@ -0,0 +1,16 @@
+package org.crip.comptabilite.comptabilitebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComptabilitebackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
